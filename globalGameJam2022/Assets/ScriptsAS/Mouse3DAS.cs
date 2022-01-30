6d09a64afdb28b9d039c1cf2a82e47e6db66d532
ASokry <anthonysokry11@gmail.com>
2022-01-29 23:57:16 -0800

    Anthony's first push

---
+++ globalGameJam2022/Assets/ScriptsAS/Mouse3DAS.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse3DAS : MonoBehaviour
+{
+    public static Mouse3DAS Instance { get; private set; }
+
+    [SerializeField] private LayerMask mouseColliderLayerMask = new LayerMask();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Physics.Raycast vs Physic2D.Raycast
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderLayerMask))
+        {
+            transform.position = raycastHit.point;
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition() => Instance.GetMouseWorldPosition_Instance();
+
+    private Vector3 GetMouseWorldPosition_Instance()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderLayerMask))
+        {
+            return raycastHit.point;
+        }
+        else
+        {
+            return new Vector3(0, 0);
+            //return new Vector3(-10,-10);
+        }
+    }
+}
